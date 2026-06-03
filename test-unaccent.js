8560e95087a7afaa4d95c3bc870911d7d74fea83
FLAVIO ALMEIDA MATOS <matos.almeida.flavio@gmail.com>
2026-06-03 10:06:44 -0300

    Removidos arquivos de teste/html desnecessarios

--- test-unaccent.js
+++
@@ -1,6 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: 'postgres://postgres:postgres@localhost:5432/rcmolinaseguros' });
-pool.query("SELECT unaccent('João') AS test")
-  .then(res => console.log(res.rows))
-  .catch(console.error)
-  .finally(() => pool.end());
