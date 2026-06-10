af85c3549d659c3dc124c93bed92445dbbe907b8
FLAVIO ALMEIDA MATOS <matos.almeida.flavio@gmail.com>
2026-06-10 18:25:20 -0300

    chore: bump version to 2.4.7 and update layout

--- fix.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let t = fs.readFileSync('src/components/configuracoes/configuracoes.tsx', 'utf8');
-t = t.replace('celular: row.celular ? \\`\\'\\${row.celular}\\` : row.celular,', 'celular: row.celular ? `\\'${row.celular}` : row.celular,');
-fs.writeFileSync('src/components/configuracoes/configuracoes.tsx', t, 'utf8');
